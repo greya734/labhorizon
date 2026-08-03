3fc88e01ea8812d24dde40adc17d9331e5447b4c
greya734 <rosajoshu@gmail.com>
2026-08-03 14:10:05 +1100

    Create api.php
    creation des routes pour l'API

---
+++ routes/api.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\RechercheApiController;
+use App\Http\Controllers\Api\AuthApiController;
+
+// Auth JSON
+Route::post('/login',  [AuthApiController::class, 'login']);
+Route::post('/logout', [AuthApiController::class, 'logout'])->middleware('auth');
+Route::get('/me',      [AuthApiController::class, 'me'])->middleware('auth');
+
+// Publiques
+Route::get('/recherches',            [RechercheApiController::class, 'index']);
+Route::get('/recherches/{recherche}', [RechercheApiController::class, 'show']);
+Route::get('/recherches/{recherche}/vulgarisations', [RechercheApiController::class, 'vulgarisations']);
+
+// Protégées
+Route::middleware('auth')->group(function () {
+    Route::post('/recherches',               [RechercheApiController::class, 'store']);
+    Route::put('/recherches/{recherche}',    [RechercheApiController::class, 'update']);
+    Route::delete('/recherches/{recherche}', [RechercheApiController::class, 'destroy']);
+});
